ef7fd6f0bdeaf4fbf49afdc2a85359806aac7a90
Shinichi Okada <okada.shin@gmail.com>
2023-08-03 07:15:21 +0200

    chore: use utils.js

--- src/routes/outline/+page.svelte
+++ src/routes/outline/+page.svelte
@@ -6,21 +6,10 @@
   import TableSearch from 'flowbite-svelte/TableSearch.svelte';
   import Icon from '$lib/IconOutline.svelte';
   import icons from '$lib/icons.js';
-  import { filterIconsByKeyword } from '../utils/filter.js'
+  import { filterIconsByKeyword, random_tailwind_color, random_hex_color_code} from '../utils.js'
 
   const outlineIcons = filterIconsByKeyword(icons, '-outline');
 
-  const random_tailwind_color = () => {
-    const colors = ['red', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink'];
-    const shades = ['300', '400', '500'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    const randomShade = shades[Math.floor(Math.random() * shades.length)];
-    return `text-${randomColor}-${randomShade} dark:text-${randomColor}-${randomShade} shrink-0`;
-  };
-  const random_hex_color_code = () => {
-    let n = (Math.random() * 0xfffff * 1000000).toString(16);
-    return '#' + n.slice(0, 6);
-  };
   const contentClass = 'rounded-lg dark:bg-neutral-950 mt-4';
   let searchTerm = '';
 
